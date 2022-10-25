d617ac0cdcca26157db57cec56344a257860f473
bmastarrr <faithbrum3@gmail.com>
2022-10-25 15:27:55 -0700

    atoi commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,12 +17,10 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
-
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -36,9 +34,8 @@ int _atoi(char *s)
 		}
 		i++;
 	}
-	
+
 	if (f == 0)
 		return (0);
-
 	return (n);
 }
