b38e3640c54427f753a9b177191a8240d4db7778
bmastarrr <faithbrum3@gmail.com>
2022-10-25 14:51:39 -0700

    c print reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,7 +4,7 @@
 * @s: string
 * return: 0
 */
-void print_rev(char*s)
+void print_rev(char *s)
 {
 	int longi = 0;
 	int o;
@@ -21,5 +21,5 @@ void print_rev(char*s)
 		s--;
 	}
 
-	_putchar('\n')
+	_putchar('\n');
 }
